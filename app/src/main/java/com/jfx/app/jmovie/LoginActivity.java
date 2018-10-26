cd4543c4e7b8c05894cda11452b6f95f0c04641b
davidtsai23 <airwaycai@163.com>
2018-10-26 17:22:36 +0800

    登录跳转

--- app/src/main/java/com/jfx/app/jmovie/LoginActivity.java
+++ app/src/main/java/com/jfx/app/jmovie/LoginActivity.java
@@ -2,12 +2,29 @@ package com.jfx.app.jmovie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView mUserName;
+    private TextView mUserPwd;
+    private Button mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mUserName = findViewById(R.id.userName);
+        mUserPwd  = findViewById(R.id.userPwd);
+        mLoginBtn = findViewById(R.id.loginBtn);
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //跳转main界面
+
+            }
+        });
     }
 }
