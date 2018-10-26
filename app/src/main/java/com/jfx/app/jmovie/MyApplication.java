9a01cf1605d00c190ed541880d5f48c2c933dff6
caiweiwei <caiweiwei@jd.com>
2018-10-26 16:52:10 +0800

    登录界面初始化

---
+++ app/src/main/java/com/jfx/app/jmovie/MyApplication.java
@@ -0,0 +1,13 @@
+package com.jfx.app.jmovie;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+    }
+}
