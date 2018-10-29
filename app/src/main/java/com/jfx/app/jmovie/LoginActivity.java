bb2b2364625496282133ebc424cab84280e3deb8
davidtsai23 <airwaycai@163.com>
2018-10-29 18:37:36 +0800

    主界面设计

--- app/src/main/java/com/jfx/app/jmovie/LoginActivity.java
+++ app/src/main/java/com/jfx/app/jmovie/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.jfx.app.jmovie;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,6 +17,12 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            //透明状态栏
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         setContentView(R.layout.activity_login);
         mUserName = findViewById(R.id.userName);
         mUserPwd  = findViewById(R.id.userPwd);
